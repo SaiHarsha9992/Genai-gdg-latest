5f3f69d95c3ad6698e9adb2f5804db17952d7dd2
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2024-10-18 11:24:41 +0530

    Update login.jsx

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -31,7 +31,7 @@ function Login()
     function Logins()
     {
         console.log("i a");
-        axios.post('/login/',data)
+        axios.post('https://genai-gdg-latest.vercel.app/login',data)
         .then((res)=>{
             setval(res.data);
             console.log(res.data);
